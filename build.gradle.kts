f0a8938b8fa30cc4e229047f6347a8e00e3976b8
Alexander Koval <kovalidis@gmail.com>
2021-03-15 16:35:30 +0200

    Hello Ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,13 @@ repositories {
     mavenCentral()
 }
 
+val ktorVersion = "1.5.2"
+
+dependencies {
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+}
+
 application {
     mainClass.set("me.alexk.MainKt")
 }
