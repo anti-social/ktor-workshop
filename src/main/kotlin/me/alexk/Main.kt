c3290822971e930f9f3b05f5e578dfc016c8d8a6
Alexander Koval <kovalidis@gmail.com>
2021-03-15 17:30:39 +0200

    Render exception

--- src/main/kotlin/me/alexk/Main.kt
+++ src/main/kotlin/me/alexk/Main.kt
@@ -3,11 +3,18 @@ package me.alexk
 import dev.evo.prometheus.ktor.metricsModule
 
 import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.content.TextContent
+import io.ktor.features.StatusPages
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import java.lang.IllegalArgumentException
 
 fun main() {
     val logger = mu.KotlinLogging.logger {}
@@ -18,10 +25,20 @@ fun main() {
     logger.info("Starting ktor application at http://$host:$port")
 
     embeddedServer(Netty, host = "localhost", port = 8080) {
+        install(StatusPages) {
+            exception<Throwable> { cause ->
+                call.respond(
+                    HttpStatusCode.InternalServerError,
+                    TextContent(cause.stackTraceToString(), ContentType.Text.Plain)
+                )
+            }
+        }
+
         metricsModule()
 
         routing {
             get("/") {
+                throw IllegalArgumentException("no argument")
                 call.respondText("Hello world!")
             }
         }
